eca1365cb8e80e630e32db96b80a42df13c7f232
chamathabeysinghe <abeysinghechamath@gmail.com>
2023-10-24 16:56:32 +0530

    add ant dataset

--- src/trackformer/datasets/tracking/factory.py
+++ src/trackformer/datasets/tracking/factory.py
@@ -6,8 +6,8 @@ from typing import Union
 
 from torch.utils.data import ConcatDataset
 
+from .mot_wrapper import MOT17Wrapper, MOTS20Wrapper, AntWrapper
 from .demo_sequence import DemoSequence
-from .mot_wrapper import MOT17Wrapper, MOT20Wrapper, MOTS20Wrapper
 
 DATASETS = {}
 
@@ -22,17 +22,39 @@ for split in ['TRAIN', 'TEST', 'ALL', '01', '02', '03', '04', '05',
             lambda kwargs, split=split, dets=dets: MOT17Wrapper(split, dets, **kwargs))
 
 
-for split in ['TRAIN', 'TEST', 'ALL', '01', '02', '03', '04', '05',
-              '06', '07', '08']:
-    name = f'MOT20-{split}'
-    DATASETS[name] = (
-        lambda kwargs, split=split: MOT20Wrapper(split, **kwargs))
-
-
 for split in ['TRAIN', 'TEST', 'ALL', '01', '02', '05', '06', '07', '09', '11', '12']:
     name = f'MOTS20-{split}'
     DATASETS[name] = (
         lambda kwargs, split=split: MOTS20Wrapper(split, **kwargs))
+
+# for split in ["OU10B3L2In_0", "OU10B1L1Out_0", "OU10B3L3In_0", "OU10B2L2In_0", "OU10B2L3Out_0", "OU10B2L2Out_0",
+#               "OU10B2L3In_0", "OU10B1L2In_0", "OU10B1L3Out_0", "OU10B1L2Out_0", "OU10B1L3In_0", "OU50B1L1In_0",
+#               "OU50B1L1Out_0", "OU50B1L2In_0", "OU50B2L2Out_0", "OU50B1L3In_0", "OU50B1L3Out_0", "OU50B1L2Out_0",
+#               "OU50B2L2In_0", "OU50B2L3In_0",
+#               "OU10B3L2Out_0", "OU10B3L3Out_0", "OU10B1L1In_0", "OU50B3L2In_0", "OU50B3L2Out_0", "OU50B3L3Out_0"]:
+for split in ["CU10L1B1In_0", "CU10L1B1Out_0", "CU25L1B1Out_0", "CU25L1B1In_0", "CU10L1B4In_0", "CU10L1B4Out_0",
+              "CU25L1B4In_0", "CU25L1B4Out_0", "CU10L1B5In_0", "CU10L1B6Out_0", "CU50L1B6In_0", "CU50L1B6Out_0",
+              "CU15L1B1Out_0", "CU20L1B1In_0", "CU15L1B4Out_0", "CU20L1B4In_0", "CU10L1B5Out_0", "CU30L1B6In_0",
+              "CU15L1B1In_0", "CU20L1B1Out_0", "CU15L1B4In_0", "CU20L1B4Out_0", "CU30L1B6Out_0", "CU10L1B6In_0",
+              "OU10B1L1Out_0", "OU50B1L1In_0", "OU10B1L2Out_0", "OU50B1L2Out_0", "OU10B1L3In_0", "OU50B1L3In_0",
+              "OU10B2L1Out_0", "OU50B2L1In_0", "OU10B2L2Out_0", "OU50B2L2In_0", "OU10B2L3In_0", "OU10B2L3Out_0",
+              "OU50B2L3In_0", "OU50B3L1In_0", "OU10B3L2In_0", "OU10B3L2Out_0", "OU50B3L2In_0", "OU10B3L3In_0",
+              "OU10B3L3Out_0", "OU50B3L3Out_0", "OU10B1L1In_0", "OU50B1L2In_0", "OU50B1L3Out_0", "OU10B2L2In_0",
+              "OU10B3L1Out_0", "OU50B3L3In_0", "OU50B1L1Out_0", "OU10B1L2In_0", "OU10B1L3Out_0", "OU10B2L1In_0",
+              "OU50B2L2Out_0", "OU10B3L1In_0", "OU50B3L2Out_0",
+              'OU10B1L1Out_0_mod_0', 'OU10B1L2Out_0_mod_0', 'OU10B1L3Out_0_mod_0', 'OU10B1L1Out_0_mod_1',
+              'OU10B1L2Out_0_mod_1', 'OU10B1L3Out_0_mod_1',
+              'colony3_small_day_2', 'colony5_small_day_2', 'colony6_small_day_1', 'colony7_small_day_1',
+                "colony3_large_day_1", "colony5_large_day1", "colony6_large_day_2", "colony7_large_day_2",
+              'task_switching_validation', 'task_switching_test', "P1320451","P1320462","P1320470","P1320480","P1320488","P1320559_1","P1320578","P1330020",
+        "P1320453","P1320464","P1320473","P1320482","P1320489","P1320563","P1320587","P1330028",
+        "P1320459","P1320467","P1320476","P1320484","P1320551","P1320565","P1320592",
+        "P1320460","P1320469","P1320478","P1320486","P1320554","P1320573","P1320783"
+              ]:
+    name = split
+    DATASETS[name] = (
+        lambda kwargs, split=split: AntWrapper(split, **kwargs))
+
 
 DATASETS['DEMO'] = (lambda kwargs: [DemoSequence(**kwargs), ])
 
--- src/trackformer/datasets/tracking/mot_wrapper.py
+++ src/trackformer/datasets/tracking/mot_wrapper.py
@@ -5,8 +5,8 @@ MOT wrapper which combines sequences to a dataset.
 from torch.utils.data import Dataset
 
 from .mot17_sequence import MOT17Sequence
-from .mot20_sequence import MOT20Sequence
 from .mots20_sequence import MOTS20Sequence
+from .ant_sequence import AntSequence
 
 
 class MOT17Wrapper(Dataset):
@@ -54,42 +54,6 @@ class MOT17Wrapper(Dataset):
         return self._data[idx]
 
 
-class MOT20Wrapper(Dataset):
-    """A Wrapper for the MOT_Sequence class to return multiple sequences."""
-
-    def __init__(self, split: str, **kwargs) -> None:
-        """Initliazes all subset of the dataset.
-
-        Keyword arguments:
-        split -- the split of the dataset to use
-        kwargs -- kwargs for the MOT20Sequence dataset
-        """
-        train_sequences = ['MOT20-01', 'MOT20-02', 'MOT20-03', 'MOT20-05',]
-        test_sequences = ['MOT20-04', 'MOT20-06', 'MOT20-07', 'MOT20-08',]
-
-        if split == "TRAIN":
-            sequences = train_sequences
-        elif split == "TEST":
-            sequences = test_sequences
-        elif split == "ALL":
-            sequences = train_sequences + test_sequences
-            sequences = sorted(sequences)
-        elif f"MOT20-{split}" in train_sequences + test_sequences:
-            sequences = [f"MOT20-{split}"]
-        else:
-            raise NotImplementedError("MOT20 split not available.")
-
-        self._data = []
-        for seq in sequences:
-            self._data.append(MOT20Sequence(seq_name=seq, dets=None, **kwargs))
-
-    def __len__(self) -> int:
-        return len(self._data)
-
-    def __getitem__(self, idx: int):
-        return self._data[idx]
-
-
 class MOTS20Wrapper(MOT17Wrapper):
     """A Wrapper for the MOT_Sequence class to return multiple sequences."""
 
@@ -118,3 +82,71 @@ class MOTS20Wrapper(MOT17Wrapper):
         self._data = []
         for seq in sequences:
             self._data.append(MOTS20Sequence(seq_name=seq, **kwargs))
+
+class AntWrapper(Dataset):
+    """A Wrapper for the MOT_Sequence class to return multiple sequences."""
+
+    def __init__(self, split: str, **kwargs) -> None:
+        """Initliazes all subset of the dataset.
+
+        Keyword arguments:
+        split -- the split of the dataset to use
+        kwargs -- kwargs for the MOT17Sequence dataset
+        """
+        # train_sequences = ["OU10B3L2In_0", "OU10B1L1Out_0", "OU10B3L3In_0", "OU10B2L2In_0", "OU10B2L3Out_0",
+        #                    "OU10B2L2Out_0", "OU10B2L3In_0", "OU10B1L2In_0", "OU10B1L3Out_0", "OU10B1L2Out_0",
+        #                    "OU10B1L3In_0", "OU50B1L1In_0", "OU50B1L1Out_0", "OU50B1L2In_0", "OU50B2L2Out_0",
+        #                    "OU50B1L3In_0", "OU50B1L3Out_0", "OU50B1L2Out_0", "OU50B2L2In_0", "OU50B2L3In_0"]
+        train_sequences = ['CU10L1B1In_0', 'CU10L1B1Out_0', 'CU25L1B1Out_0', 'CU25L1B1In_0',
+             'CU15L1B4In_0', 'CU15L1B4Out_0', 'CU20L1B4In_0', 'CU20L1B4Out_0',
+             'CU50L1B6In_0', 'CU50L1B6Out_0',
+             'CU10L1B5In_0', 'CU10L1B6Out_0', 'CU15L1B1In_0', 'CU20L1B1Out_0', 'CU10L1B4In_0', 'CU25L1B4In_0', 'CU10L1B6In_0', 'CU10L1B5Out_0', 'CU30L1B6Out_1', 'CU30L1B6Out_2', 'CU30L1B6Out_3',
+                           'OU10B1L1Out_0_mod_0', 'OU10B1L2Out_0_mod_0', 'OU10B1L3Out_0_mod_0', 'OU10B1L1Out_0_mod_1',
+                           'OU10B1L2Out_0_mod_1', 'OU10B1L3Out_0_mod_1','colony3_small_day_2', 'colony5_small_day_2', 'colony6_small_day_1', 'colony7_small_day_1', "colony3_large_day_1", "colony5_large_day1", "colony6_large_day_2", "colony7_large_day_2",
+                           ]
+        # test_sequences = ["OU10B3L2Out_0", "OU10B3L3Out_0", "OU10B1L1In_0", "OU50B3L2In_0", "OU50B3L2Out_0", "OU50B3L3Out_0"]
+        test_sequences = ['CU15L1B1Out_0', 'CU20L1B1In_0', 'CU10L1B4Out_0', 'CU25L1B4Out_0', 'CU30L1B6In_0', 'CU30L1B6Out_0',
+                          'OU10B1L1Out_0_mod_0', 'OU10B1L1Out_0_mod_1','colony3_small_day_2', 'colony5_small_day_2', 'colony6_small_day_1', 'colony7_small_day_1']
+        all_sequences = ["CU10L1B1In_0", "CU10L1B1Out_0", "CU25L1B1Out_0", "CU25L1B1In_0", "CU10L1B4In_0", "CU10L1B4Out_0",
+              "CU25L1B4In_0", "CU25L1B4Out_0", "CU10L1B5In_0", "CU10L1B6Out_0", "CU50L1B6In_0", "CU50L1B6Out_0",
+              "CU15L1B1Out_0", "CU20L1B1In_0", "CU15L1B4Out_0", "CU20L1B4In_0", "CU10L1B5Out_0", "CU30L1B6In_0",
+              "CU15L1B1In_0", "CU20L1B1Out_0", "CU15L1B4In_0", "CU20L1B4Out_0", "CU30L1B6Out_0", "CU10L1B6In_0",
+              "OU10B1L1Out_0", "OU50B1L1In_0", "OU10B1L2Out_0", "OU50B1L2Out_0", "OU10B1L3In_0", "OU50B1L3In_0",
+              "OU10B2L1Out_0", "OU50B2L1In_0", "OU10B2L2Out_0", "OU50B2L2In_0", "OU10B2L3In_0", "OU10B2L3Out_0",
+              "OU50B2L3In_0", "OU50B3L1In_0", "OU10B3L2In_0", "OU10B3L2Out_0", "OU50B3L2In_0", "OU10B3L3In_0",
+              "OU10B3L3Out_0", "OU50B3L3Out_0", "OU10B1L1In_0", "OU50B1L2In_0", "OU50B1L3Out_0", "OU10B2L2In_0",
+              "OU10B3L1Out_0", "OU50B3L3In_0", "OU50B1L1Out_0", "OU10B1L2In_0", "OU10B1L3Out_0", "OU10B2L1In_0",
+              "OU50B2L2Out_0", "OU10B3L1In_0", "OU50B3L2Out_0",
+              'OU10B1L1Out_0_mod_0', 'OU10B1L2Out_0_mod_0', 'OU10B1L3Out_0_mod_0', 'OU10B1L1Out_0_mod_1',
+              'OU10B1L2Out_0_mod_1', 'OU10B1L3Out_0_mod_1',
+             'colony3_small_day_2', 'colony5_small_day_2', 'colony6_small_day_1', 'colony7_small_day_1',
+             "colony3_large_day_1", "colony5_large_day1", "colony6_large_day_2", "colony7_large_day_2",
+             'task_switching_validation', 'task_switching_test',
+             "P1320451", "P1320462", "P1320470", "P1320480", "P1320488", "P1320559_1", "P1320578",
+             "P1330020",
+             "P1320453", "P1320464", "P1320473", "P1320482", "P1320489", "P1320563", "P1320587", "P1330028",
+             "P1320459", "P1320467", "P1320476", "P1320484", "P1320551", "P1320565", "P1320592",
+             "P1320460", "P1320469", "P1320478", "P1320486", "P1320554", "P1320573", "P1320783",
+                         ]
+        if split == "TRAIN":
+            sequences = train_sequences
+        elif split == "TEST":
+            sequences = test_sequences
+        elif split == "ALL":
+            sequences = train_sequences + test_sequences
+            sequences = sorted(sequences)
+        elif split in all_sequences:
+            sequences = [split]
+        else:
+            raise NotImplementedError("MOT17 split not available.")
+
+        self._data = []
+        for seq in sequences:
+            self._data.append(AntSequence(seq_name=seq, **kwargs))
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __getitem__(self, idx: int):
+        return self._data[idx]
+
